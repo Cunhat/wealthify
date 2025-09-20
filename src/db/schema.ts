bbfd106329fa633e331f84937d5024b5bb1d39e0
Tiago Cunha <tiago92cunha@gmail.com>
2025-09-20 21:12:46 +0100

    Create recurring transactions db schema

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -231,3 +231,20 @@ export const balanceAccountHistoryRelations = relations(
 		}),
 	}),
 );
+
+export const recurringTransaction = pgTable("recurring_transaction", {
+	id: text("id")
+		.primaryKey()
+		.$defaultFn(() => crypto.randomUUID()),
+	amount: decimal("amount").notNull(),
+	description: text("description"),
+	firstOccurrence: timestamp("first_occurrence").notNull(),
+	frequency: text("frequency").notNull(),
+	createdAt: timestamp("created_at").notNull(),
+	updatedAt: timestamp("updated_at")
+		.$defaultFn(() => /* @__PURE__ */ new Date())
+		.notNull(),
+	userId: text("user_id")
+		.notNull()
+		.references(() => user.id, { onDelete: "cascade" }),
+});
