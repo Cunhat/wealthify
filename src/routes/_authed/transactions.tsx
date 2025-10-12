bcf9024b6e4b4871b6f76f46532f89f8bfaef761
Tiago Cunha <tiago92cunha@gmail.com>
2025-10-12 18:17:04 +0100

    Improve code

--- src/routes/_authed/transactions.tsx
+++ src/routes/_authed/transactions.tsx
@@ -1,5 +1,5 @@
 import TransactionsView from "@/modules/transactions/views/transactions-view";
-import { createFileRoute, redirect } from "@tanstack/react-router";
+import { createFileRoute } from "@tanstack/react-router";
 import { z } from "zod";
 
 export const Route = createFileRoute("/_authed/transactions")({
@@ -8,18 +8,12 @@ export const Route = createFileRoute("/_authed/transactions")({
 		category: z.array(z.string()).optional().catch([]),
 		account: z.array(z.string()).optional().catch([]),
 	}),
-	beforeLoad: async ({ context, search }) => {
-		if (!context.user) {
-			throw redirect({ to: "/login" });
-		}
-
+	beforeLoad: async ({ search }) => {
 		return {
 			search: search,
 		};
 	},
-	loader: async (test) => {
-		const { context } = test;
-
+	loader: async ({ context }) => {
 		await context.queryClient.prefetchQuery(
 			context.trpc.transactions.listTransactions.queryOptions({
 				limit: 100,
